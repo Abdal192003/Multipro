675bfa673ca32e9b67a768771ab61054e0e9abbc
ABDAL AHMAD KHAN <117366584+Abdal192003@users.noreply.github.com>
2023-02-26 23:48:49 +0530

    Add files via upload

---
+++ multiplication table .py
@@ -0,0 +1,8 @@
+# program="Multi-pro"
+class multiple:
+    user=int(input("Enter a Number"))
+    def multiple(self):
+        for i in range(1,11):
+         print(f"{self.user} X {i}={self.user*i}")
+a=multiple()
+a.multiple()
